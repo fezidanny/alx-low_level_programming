961e2e0c1c4881fb281cba3d06554e4ae7607ed8
fezidanny <106951571+fezidanny@users.noreply.github.com>
2022-07-06 20:48:22 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,14 +14,14 @@ void jack_bauer(void)
 
 	for (H = 0; H <= 23; H++)
 	{
-	for (M = 0; M <= 59; M++)
-	{
-		_putchar((H / 10) + '0');
-		_putchar((H % 10) + '0');
-		_putchar(':');
-		_putchar((M / 10) + '0'):
-		_putchar((M % 10) + '0');
-		_putchar('\n');
-	}
+		for (M = 0; M <= 59; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
