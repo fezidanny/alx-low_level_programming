a6c0de39d03dd5fa3d610f1f492a6556344b3629
fezidanny <106951571+fezidanny@users.noreply.github.com>
2022-07-12 19:29:36 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,3 +6,18 @@
  *
  * Return: nothing
  */
+void rev_string(char *s)
+{
+	int i = 0, j;
+	char tmp;
+
+	for (; s[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < (i / 2); j++)
+	{
+		tmp = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = tmp;
+	}
+}
