bae00a5ee1d136c92534f7991becd2c9d2dc49f7
fezidanny <106951571+fezidanny@users.noreply.github.com>
2022-07-12 15:09:21 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter
- *  and updates the value it points to to 98.
- *  @n: this is the pointer argument passed in.
+ * and updates the value it points to to 98.
+ * @n: this is the pointer argument passed in.
  *
- *  Return: void.
+ * Return: void.
  */
 void reset_to_98(int *n)
 {
