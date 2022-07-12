e4ad4fd6aefa7bbd2e19c70f0faead4be4067dad
Chibuikem Ifezue <danielifezue@gmail.com>
2022-07-12 11:47:57 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+
+/**
+ * _strycpy - copies a string pointed to by src,
+ * including the /0 character, to the buffer
+ * pointed to by dest.
+ * @dest: the copied string
+ * @src: the pointer to the string to be copied
