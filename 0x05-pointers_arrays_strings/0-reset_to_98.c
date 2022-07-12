7abc9383c539c26bb542699cdbcfc62a7625f4d1
fezidanny <106951571+fezidanny@users.noreply.github.com>
2022-07-12 15:23:13 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "holberton.h"
 /**
  * reset_to_98 - Updates the value it points to 98
@@ -10,6 +9,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-=======
-
->>>>>>> 218e976fc7cb23cb7e3edea682e2521f7b3be316
