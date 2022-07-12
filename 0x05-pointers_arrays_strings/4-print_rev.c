506e7381be5dcc7b202e90748e336bb617189cf4
fezidanny <106951571+fezidanny@users.noreply.github.com>
2022-07-12 19:07:30 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,3 +5,15 @@
  * @s: the string to be manipulated.
  *
  * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (i -= 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
