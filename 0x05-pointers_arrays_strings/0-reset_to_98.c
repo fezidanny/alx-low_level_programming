9bac55dbf63161339bef8c6340b070054118cff8
Chibuikem Ifezue <danielifezue@gmail.com>
2022-07-12 07:36:34 -0700

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
 #include "holberton.h"
+
 /**
  * reset_to_98 - Updates the value it points to 98
  * @n: pointer to n
  *
  * Return: void
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
-=======
-
->>>>>>> 218e976fc7cb23cb7e3edea682e2521f7b3be316
