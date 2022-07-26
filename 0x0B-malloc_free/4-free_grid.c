46f962b4a1de0e18c46c02d292122aed636ea4d1
fezidanny <106951571+fezidanny@users.noreply.github.com>
2022-07-26 17:37:36 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,2 +1,20 @@
-#include "holberton.h"
 #include <stdlib.h>
+
+/**
+ * free_grid - alloc_grid
+ * @grid: pointer to 2D arrays
+ * @height: array
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	height--;
+
+	while (height >= 0)
+	{
+		free(*(grid + height));
+		height--;
+	}
+	free(grid);
+}
